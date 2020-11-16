3a2397c05d667fbf45efaf9e446d2154dd3423d3
NicolaiBomholt <nicolaibomholt@gmail.com>
2020-11-16 13:52:40 +0100

    setup work

---
+++ a-new-start/src/Classes/SkeletClass.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class SkeletClass extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hej</h1>
+            </div>
+        )
+    }
+}
---
+++ a-new-start/src/Functions/SkeletFunc.js
@@ -0,0 +1,13 @@
+import React, { useEffect } from "react";
+
+export default function SkeletFunc() {
+
+    return (
+        <div>
+            <h1>
+                Hej
+            </h1>
+        </div>
+    )
+    
+}
