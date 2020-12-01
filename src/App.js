092db7b788b6f34cdbc1273d9e0bd033438fc362
NicolaiBomholt <nicolaibomholt@gmail.com>
2020-12-01 14:53:37 +0100

    abc

--- src/App.js
+++ src/App.js
@@ -7,10 +7,17 @@ import UserProvider from "./providers/UserProvider";
 import ProfilePage from "./Functions/ProfilePage";
 import { UserContext } from "./providers/UserProvider";
 import MenuContainer from "./Functions/MenuContainer";
+import { Link, navigate } from "@reach/router";
+
 function App() {
   const user = useContext(UserContext);
+  const start = () => {
+    navigate("/base");
+  }
   return (
+    
     <UserProvider>
+      <button onClick = {() => start()}>Start</button>
       <MenuContainer user = {user}></MenuContainer>
       <Application />
     </UserProvider>
